75e325592ba759441abdef5cd1fb12ff05ce845a
Giovanni Cardenas <GioHiCar@gmail.com>
2024-09-24 23:48:30 -0500

    completed prep1 and 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+
+        string letter = "";
+        
+        if (grade >= 90){
+            
+        }
+        else if (grade >= 80){
+            letter = "A";
+        }
+        else if (grade >= 70){
+            letter = "B";
+        }
+        else if (grade >= 60){
+            letter = "C";
+        }
+        else{
+            letter = "F";
+        }
+        
+        if (grade >= 70){
+            Console.WriteLine($"Congrats, you passed with {letter}");
+        }
+        else{
+            Console.WriteLine($"Sorry, you have failed with {letter}");
+        }
     }
 }
