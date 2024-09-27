1f1a578cc7f96dc91f062db6516bb59d73cc9f04
Giovanni Cardenas <GioHiCar@gmail.com>
2024-09-26 19:10:41 -0500

    completed Abstract Learning

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Mcdonald's";
+        job1._jobTitle = "Burger Flipper";
+        job1._startYear = 2022;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._company = "Amazon";
+        job2._jobTitle = "DSP Driver";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Giovanni Cardenas";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
     }
 }
