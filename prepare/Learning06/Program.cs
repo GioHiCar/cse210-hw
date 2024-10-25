7808431e612b8d195f3a2a7b25f3838ca589788e
Giovanni Cardenas <GioHiCar@gmail.com>
2024-10-25 15:03:35 -0500

    finished shapes learning

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+        Square square = new Square("green", 5);
+        shapes.Add(square);
+        
+        Rectangle rectangle = new Rectangle("blue", 5, 6);
+        shapes.Add(rectangle);
+
+        Circle circle = new Circle("yellow", 5);
+        shapes.Add(circle);
+
+        foreach(Shape s in shapes){
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape had an area of {area}");
+        }
     }
 }
